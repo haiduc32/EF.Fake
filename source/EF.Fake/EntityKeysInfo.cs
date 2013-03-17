d6fe3d518435c2d9da627332075bd3f931dd3354
Radu Pascal <haiduc32@gmail.com>
2013-03-17 21:26:32 +0200

    adding new types

---
+++ source/EF.Fake/EntityKeysInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Fake
+{
+	public class EntityKeysInfo
+	{
+		public Type Entity { get; set; }
+		public List<KeyPropertyInfo> Keys { get; set; }
+	}
+}
---
+++ source/EF.Fake/KeyPropertyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Fake
+{
+	public class KeyPropertyInfo
+	{
+		public string Name { get; set; }
+		public bool IsIdentity { get; set; }
+	}
+}
